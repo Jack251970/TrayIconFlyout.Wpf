fb58cea995ee273b0b91125829ef7d26b40b4a0f
Jack251970 <1160210343@qq.com>
2026-02-16 16:12:08 +0800

    Remove unused WNDPROC delegate and IID definitions
    Removed the WNDPROC delegate and the IID static partial class, including GUIDs for IDesktopWindowXamlSourceNative2 and ICoreWindowInterop, from ManualDefinitions.cs. Also cleaned up related using directives and removed references to these interfaces from NativeMethods.txt, as they are no longer needed in the project.

--- src/TrayIconFlyout.Wpf/Helpers/ManualDefinitions.cs
+++ src/TrayIconFlyout.Wpf/Helpers/ManualDefinitions.cs
@@ -1,16 +1,11 @@
 ﻿// Copyright (c) Files Community
 // Licensed under the MIT License.
 
-using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Windows.Win32.Foundation;
 
 namespace Windows.Win32
 {
-	[UnmanagedFunctionPointer(CallingConvention.Winapi)]
-	internal delegate LRESULT WNDPROC([In] HWND hWnd, [In] uint uMsg, [In] WPARAM wParam, [In] LPARAM lParam);
-
 	internal static class ManualDefinitions
 	{
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -35,21 +30,6 @@ namespace Windows.Win32
 		public static ushort HIWORD(nint l)
 		{
 			return ((ushort)((((nuint)(l)) >> 16) & 0XFFFF));
-		}
-	}
-
-	internal static partial class IID
-	{
-		internal static ref readonly Guid IID_IDesktopWindowXamlSourceNative2
-		{
-			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			get => ref MemoryMarshal.AsRef<Guid>([0xC7, 0xD8, 0xDC, 0xE3, 0x57, 0x30, 0x92, 0x46, 0x99, 0xC3, 0x7B, 0x77, 0x20, 0xAF, 0xDA, 0x31]);
-		}
-
-		internal static ref readonly Guid IID_ICoreWindowInterop
-		{
-			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			get => ref MemoryMarshal.AsRef<Guid>([0x29, 0x4A, 0xD6, 0x45, 0x3E, 0xA6, 0xB6, 0x4C, 0xB4, 0x98, 0x57, 0x81, 0xD2, 0x98, 0xCB, 0x4F]);
 		}
 	}
 }
