00d15cee019ca800e477ab9407bf9597168c8fda
Jack251970 <1160210343@qq.com>
2026-02-17 12:10:10 +0800

    Add BackdropColorHelpers and update target frameworks
    Added BackdropColorHelpers.cs with static methods for retrieving dark, light, and accented background colors using Windows UI settings. Updated TrayIconFlyout.Wpf.csproj to target net9.0-windows10.0.19041.0 and net10.0-windows10.0.19041.0 for improved Windows version specificity.

---
+++ src/TrayIconFlyout.Wpf/Helpers/BackdropColorHelpers.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 0x5BFA. All rights reserved.
+// Licensed under the MIT license.
+
+using System.Windows.Media;
+using Windows.UI.ViewManagement;
+
+namespace U5BFA.Libraries
+{
+	internal static class BackdropColorHelpers
+	{
+        private static UISettings UISettings => field ??= new UISettings();
+
+        internal static Color GetDarkBackgroundColor()
+		{
+			return Color.FromArgb(0xFF, 0x1C, 0x1C, 0x1C);
+		}
+
+		internal static Color GetLightBackgroundColor()
+		{
+			return Color.FromArgb(0xFF, 0xEE, 0xEE, 0xEE);
+		}
+
+		internal static Color GetAccentedBackgroundColor()
+		{
+            var systemAccentColorDark2 = UISettings.GetColorValue(UIColorType.AccentDark2);
+            return Color.FromArgb(
+				systemAccentColorDark2.A,
+				systemAccentColorDark2.R,
+				systemAccentColorDark2.G,
+				systemAccentColorDark2.B);
+        }
+	}
+}
